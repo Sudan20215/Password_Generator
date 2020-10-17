6ca99f8abd5440ee9403af3dd29cb578d9156b6a
sudan bhusal <sudanbhusal20215@gmail.com>
2020-10-17 15:45:56 -0700

    comments added

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // Assignment Code
-
+// program started
 let generatepasswordBtn = document.querySelector("#generate");
+// All the element are defined
 let onlyupperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 let onlylowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let onlynumbers=["0","1","2","3","4","5","6","7","8","9","0"]
 let onlysymbols=["!","@","#","$","%","&","^","*","+","?"]
+// welcomes you on the page
 window.onload = alert("Welcome! Please click 'Generate password' to start!");
 function generateOptions()
 {
+    // prompt the user to enter password size
 
  let length= parseInt(prompt("how many characters will your password contains?"));
   if (length<8)
@@ -20,6 +23,7 @@ function generateOptions()
     alert("Invalid entry!!! make sure its less that 128 character");
     return;
   }
+//   Defining the element
   let includesymbols= confirm("Do you wants to have symbols in your password?");
   let includeupperCase= confirm("Do you wants to have uppercase in your password?");
   let includelowerCase= confirm("Do you wants to have lowercase in your password?");
@@ -44,34 +48,35 @@ return allvalue;
 
 // Write password to the #password input
 function writepassword(){
- let options=generateOptions();
- if (!options) {
+ let confirm=generateOptions();
+ if (!confirm) {
     return;
  }
+//  help to extract the password
   let passwordPool=[];
   console.log(passwordPool)
-  if (options.lowerCase){
+  if (confirm.lowerCase){
     for (let i = 0; i < onlylowerCase.length; i++) {
       passwordPool.push(onlylowerCase[i]);
     }
   }
-  if (options.upperCase){
+  if (confirm.upperCase){
     for (let i = 0; i <onlyupperCase.length; i++) {
        passwordPool.push(onlyupperCase[i]);
     }
   }
-  if (options.numeric){
+  if (confirm.numeric){
     for (let i = 0; i <onlynumbers.length; i++) {
         passwordPool.push(onlynumbers[i]);
     }
   }
-  if (options.specialcharacters){
+  if (confirm.specialcharacters){
     for (let i = 0; i < onlysymbols.length; i++) {
         passwordPool.push(onlysymbols[i]);
   }
 }
  let lastpassword=[];
- for (let i = 0; i < options.length; i++) {
+ for (let i = 0; i < confirm.length; i++) {
    let pickrandomly =Math.floor(Math.random()*Math.floor(passwordPool.length));
    lastpassword.push(passwordPool[pickrandomly]);
  }
@@ -80,12 +85,7 @@ function writepassword(){
  console.log(finalpassword);
  document.getElementById("password").value = finalpassword;
   } 
-  function copyPassword() {
-    document.getElementById("writepassword").select();
-    document.execCommand("Copy");
-    alert("Password copied to clipboard!");
-}
-
+  
+ 
 // Add event listener to generate button
 generatepasswordBtn.addEventListener("click", writepassword);
-
