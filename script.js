d37063b215c75a5e6fe5bebcfd57ebebc7a57bc6
sudan bhusal <sudanbhusal20215@gmail.com>
2020-10-17 14:29:31 -0700

    if-else added with cipboard

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let onlyupperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 let onlylowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let onlynumbers=["0","1","2","3","4","5","6","7","8","9","0"]
 let onlysymbols=["!","@","#","$","%","&","^","*","+","?"]
-unction generateOptions()
+function generateOptions()
 {
 
  let length= parseInt(prompt("how many characters will your password contains?"));
